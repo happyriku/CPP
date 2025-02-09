e6c4932c6437dccee6d1277422a250eb31c3c355
Riku Ishibashi <rishibas@c3r5s2.42tokyo.jp>
2025-02-09 17:16:32 +0900

    ADD機能を一応動作するところまでは作成

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -1,43 +1,73 @@
 #include <iostream>
 #include <vector>
 
+class Contact
+{
+	private:
+		std::string	firstname;
+		std::string	lastname;
+		std::string	nickname;
+		std::string	phoneNumber;
+	
+	public:
+		Contact(std::string f, std::string l, std::string n, std::string p)
+			: firstname(f), lastname(l), nickname(n), phoneNumber(p) {}
+		
+		void	printcontact()
+		{
+			std::cout << "first name : " << firstname << std::endl;
+			std::cout << "last name : " << lastname << std::endl;
+			std::cout << "nickname : " << nickname << std::endl;
+			std::cout << "phone number : " << phoneNumber << std::endl;
+		}
+
+};
+
 class PhoneBook
 {
 	private:
-		std::vector<std::vector<std::string>> data;
+		std::vector<Contact> contacts;
 	public:
-		void	addContact()
+		void	addContact(std::string firstname, std::string lastname,
+						std::string nickname, std::string phoneNumber)
 		{
-			std::string firstname, lastname, nickname, phoneNumber;
-
-			std::cout << "Please enter your first name ." << std::endl;
-			std::cin >> firstname;
-
-			std::cout << "Please enter your last name ." << std::endl;
-			std::cin >> lastname;
-
-			std::cout << "Please enter your nickname ." << std::endl;
-			std::cin >> nickname;
-
-			std::cout << "Please enter your phone number ." << std::endl;
-			std::cin >> phoneNumber;
-
-			data.push_back({firstname, lastname, nickname, phoneNumber});
+			contacts.push_back(Contact(firstname, lastname, nickname, phoneNumber));
 		};
-		
+
+		void	printcontacts()
+		{
+			for (int i = 0; i < contacts.size(); i++)
+				contacts[i].printcontact();
+		}
 };
+
 
 int main(void)
 {
-	std::string	cmd;
 	PhoneBook	phonebook;
+	std::string	cmd;
+	std::string firstname;
+	std::string	lastname;
+	std::string	nickname;
+	std::string	phoneNumber;
 
 	std::cout << "Please enter one of the following three commands: ADD, SEARCH, or EXIT." << std::endl;
 	std::getline(std::cin, cmd);
 	while (cmd != "EXIT")
 	{
 		if (cmd == "ADD")
-			phonebook.addContact();
+		{
+			std::cout << "Enter your first name ." << std::endl;
+			std::cin >> firstname;
+			std::cout << "Enter your last name ." << std::endl;
+			std::cin >> lastname;
+			std::cout << "Enter your nickname ." << std::endl;
+			std::cin >> nickname;
+			std::cout << "Enter your phone number ." << std::endl;
+			std::cin >> phoneNumber;
+			phonebook.addContact(firstname, lastname, nickname, phoneNumber);
+		}
+		phonebook.printcontacts();
 	}
 	return (0);
 }
