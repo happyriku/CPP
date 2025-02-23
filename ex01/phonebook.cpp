cf4fcfbd2b75911c8435127d7604b0d38e47e4cb
Riku Ishibashi <rishibas@c3r2s8.42tokyo.jp>
2025-02-23 16:12:19 +0900

    codeをきれいにした。あとは細かい所のエラーハンドリングをする

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -17,14 +17,22 @@ class Contact
 	public:
 		Contact() : firstname(""), lastname(""), nickname(""), phoneNumber(""), secret("") {}
 		
-		void	setContactInfo(std::string f, std::string l,
-					std::string n, std::string p, std::string s)
+		void	init()
 		{
-			firstname = f;
-			lastname = l;
-			nickname = n;
-			phoneNumber = p;
-			secret = s;	
+			std::cout << "Enter your first name ." << std::endl;
+			std::getline(std::cin, firstname);
+
+			std::cout << "Enter your last name ." << std::endl;
+			std::getline(std::cin, lastname);
+
+			std::cout << "Enter your nickname ." << std::endl;
+			std::getline(std::cin, nickname);
+
+			std::cout << "Enter your phone number ." << std::endl;
+			std::getline(std::cin, phoneNumber);
+
+			std::cout << "Tell me your darkest secret ." << std::endl;
+			std::getline(std::cin, secret);
 		}
 
 		bool	isEmpty()
@@ -35,24 +43,31 @@ class Contact
 			return (false);			
 		}
 
+		std::string	truncate10character(std::string	info)
+		{
+			if (info.length() >= 10)
+				info.at(9) = '.';
+			return (info);
+		}
+
 		void	printContacts(int i)
 		{
 			std::cout << "-----------------------------------------------------------------" << std::endl;
 			std::cout << "|" << std::setw(15) << std::right << i << "|";
-			std::cout << std::setw(15) << std::right << firstname.substr(0, 10) << "|";
-			std::cout << std::setw(15) << std::right << lastname.substr(0, 10) << "|";
-			std::cout << std::setw(15) << std::right << nickname.substr(0, 10) << "|" << std::endl;
+			std::cout << std::setw(15) << std::right << truncate10character(firstname.substr(0, 10)) << "|";
+			std::cout << std::setw(15) << std::right << truncate10character(lastname.substr(0, 10)) << "|";
+			std::cout << std::setw(15) << std::right << truncate10character(nickname.substr(0, 10)) << "|" << std::endl;
 		}
 
 		void	printContactsByIndex(int i)
 		{
 			std::cout << "------------------------------------------------------------------------------------------------" << std::endl;
 			std::cout << "|" << std::setw(15) << std::right << i << "|";
-			std::cout << std::setw(15) << std::right << firstname.substr(0, 10) << "|";
-			std::cout << std::setw(15) << std::right << lastname.substr(0, 10) << "|";
-			std::cout << std::setw(15) << std::right << nickname.substr(0, 10) << "|";
-			std::cout << std::setw(15) << std::right << phoneNumber.substr(0, 10) << "|";
-			std::cout << std::setw(15) << std::right << secret.substr(0, 10) << "|" << std::endl;
+			std::cout << std::setw(15) << std::right << truncate10character(firstname.substr(0, 10)) << "|";
+			std::cout << std::setw(15) << std::right << truncate10character(lastname.substr(0, 10)) << "|";
+			std::cout << std::setw(15) << std::right << truncate10character(nickname.substr(0, 10)) << "|";
+			std::cout << std::setw(15) << std::right << truncate10character(phoneNumber.substr(0, 10)) << "|";
+			std::cout << std::setw(15) << std::right << truncate10character(secret.substr(0, 10)) << "|" << std::endl;
 			std::cout << "------------------------------------------------------------------------------------------------" << std::endl;
 		}
 };
@@ -61,10 +76,11 @@ class PhoneBook
 {
 	private:
 		Contact contacts[8];
+		int		index;
 
 	public:
-	
-		int		ElemSize()
+
+		int	ElemSize()
 		{
 			int	i = -1;
 
@@ -74,20 +90,20 @@ class PhoneBook
 			return (i);
 		}
 
-		std::string	replace(std::string info)
+		void	add(int num)
 		{
-			if (info.length() >= 10)
-				info.at(9) = '.';
-			return (info);
+			contacts[num % CONTACT_SIZE].init();
+			std::cout << "Successfully registered contact details!" << std::endl;
 		}
 
-		void	addContact(std::string firstname, std::string lastname,
-			std::string nickname, std::string phoneNumber, std::string secret, int num)
-			{
-				contacts[num % CONTACT_SIZE].setContactInfo(replace(firstname), replace(lastname),
-							replace(nickname), replace(phoneNumber), replace(secret));
-				std::cout << "Successfully registered contact details!" << std::endl;
-			}
+		void	search()
+		{
+			printListItems();
+			std::cout << "Enter the index of entry ." << std::endl;
+			std::cin >> index;
+			std::cin.ignore();
+			printContactOfIndex(index);
+		}
 
 		void	printListItems()
 		{
@@ -101,17 +117,6 @@ class PhoneBook
 			std::cout << "-----------------------------------------------------------------" << std::endl;
 		}
 
-		void	printListItemsByIndex()
-		{
-			std::cout << "------------------------------------------------------------------------------------------------" << std::endl;
-			std::cout << "|" << std::setw(15) << "index" << "|";
-			std::cout << std::setw(15) << "first name" << "|";
-			std::cout << std::setw(15) << std::right << "last name" << "|";
-			std::cout << std::setw(15) << std::right << "nickname" << "|";
-			std::cout << std::setw(15) << std::right << "phone number" << "|";
-			std::cout << std::setw(15) << std::right << "darkest secret" << "|" << std::endl;
-		}
-
 		void	printContactOfIndex(int index)
 		{
 			if (index <= 0 || ElemSize() < index)
@@ -119,7 +124,13 @@ class PhoneBook
 				std::cout << "index is out of range or wrong ." << std::endl;
 				return ;
 			}
-			printListItemsByIndex();
+			std::cout << "------------------------------------------------------------------------------------------------" << std::endl;
+			std::cout << "|" << std::setw(15) << "index" << "|";
+			std::cout << std::setw(15) << "first name" << "|";
+			std::cout << std::setw(15) << std::right << "last name" << "|";
+			std::cout << std::setw(15) << std::right << "nickname" << "|";
+			std::cout << std::setw(15) << std::right << "phone number" << "|";
+			std::cout << std::setw(15) << std::right << "darkest secret" << "|" << std::endl;
 			contacts[index - 1].printContactsByIndex(index);
 		}
 };
@@ -128,12 +139,6 @@ int main(void)
 {
 	PhoneBook	phonebook;
 	std::string	cmd;
-	std::string firstname;
-	std::string	lastname;
-	std::string	nickname;
-	std::string	phoneNumber;
-	std::string secret;
-	int			index;
 	int			count = 0;
 
 	while (1)
@@ -143,28 +148,9 @@ int main(void)
 		if (cmd == "EXIT")
 			return (0);
 		else if (cmd == "ADD")
-		{
-			std::cout << "Enter your first name ." << std::endl;
-			std::getline(std::cin, firstname);
-			std::cout << "Enter your last name ." << std::endl;
-			std::getline(std::cin, lastname);
-			std::cout << "Enter your nickname ." << std::endl;
-			std::getline(std::cin, nickname);
-			std::cout << "Enter your phone number ." << std::endl;
-			std::getline(std::cin, phoneNumber);
-			std::cout << "Tell me your darkest secret ." << std::endl;
-			std::getline(std::cin, secret);
-			phonebook.addContact(firstname, lastname,
-						nickname, phoneNumber, secret, count++);
-		}
+			phonebook.add(count++);
 		else if (cmd == "SEARCH")
-		{
-			phonebook.printListItems();
-			std::cout << "Enter the index of entry ." << std::endl;
-			std::cin >> index;
-			std::cin.ignore();
-			phonebook.printContactOfIndex(index);
-		}
+			phonebook.search();
 	}
 	return (0);
 }
